b6cc0a081954ff315903043902f51e6455601d89
nnlgsakib <nlgarts@outlook.com>
2024-07-16 23:29:22 +0600

    updated logo uri and pkg  version

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -4,7 +4,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <img
-      src="https://www.mindchainswap.com/logo.png" // Replace with the path to your PNG file
+      src="https://i.postimg.cc/wT81Mxhj/mindchainsw-ap-256-01.png" // Replace with the path to your PNG file
       alt="Logo" // Provide an appropriate alt text for accessibility
       {...props} // Spread any additional props passed to the component
     />
